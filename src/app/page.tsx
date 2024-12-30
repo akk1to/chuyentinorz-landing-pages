4cca075530978018ec8e8afbbfb719ce3f59241d
akk1to.dev <dragongm1981@gmail.com>
2024-12-30 12:00:25 +0700

    Add Speed Insights system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import styles from './page.module.css'
 import { Analytics } from "@vercel/analytics/react";  
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import HomeHero from "@/components/landing/HomeHero";
 import AppHeader from "@/components/shared/Header";
 import ThemeProvider from "@/components/providers/ThemeProvider";
@@ -16,6 +17,7 @@ export default function Home() {
         <Story/>
         <Careers/>
         <Analytics/>
+        <SpeedInsights/>
       </ThemeProvider>
     </main>
   )
